b75ffc7d55f9190b3b6e79ecdba6bf48938597b8
Amirreza Sokhankhosh <amirrezasokhankhosh79@gmail.com>
2020-12-04 14:57:19 +0330

    bug fixed

--- show.py
+++ show.py
@@ -22,7 +22,7 @@ for i in output_lines:
     j = j + 1
 
 plt.scatter([1 , 2 , 3 , 4] , times)
-plt.suptitle("1 is (" + configsFirst[0] + " , " + configsSecond[0] + " ) -  2 is ( " + configsFirst[1] + " , " + configsSecond[1] + " ) - 3 is ( " + configsFirst[0] + " , " + configsSecond[0] + " ) - 4 is ( " + configsFirst[0] + " , " + configsSecond[0] + " ).")
+plt.suptitle("1 is (" + configsFirst[0] + " , " + configsSecond[0] + " ) -  2 is ( " + configsFirst[1] + " , " + configsSecond[1] + " ) - 3 is ( " + configsFirst[2] + " , " + configsSecond[2] + " ) - 4 is ( " + configsFirst[3] + " , " + configsSecond[3] + " ).")
 plt.show()
 
 
